3a8e05d765cfe9926db9996585302f17b98d748a
Cesar Manuel Baquiro Munoz <cesar.baquiro@globant.com>
2023-01-17 11:17:44 -0500

    Ya funciona el formulario, imprime lo ingresado por consola

--- src/app/Usuario/agregar/agregar.component.ts
+++ src/app/Usuario/agregar/agregar.component.ts
@@ -10,32 +10,24 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./agregar.component.css'],
 })
 export class AgregarComponent implements OnInit {
-  public form!: FormGroup;
-  public formController!: FormControl;
-  nombreInput: string = '';
-  emailInput: string = '';
-  prioridadInput: number = 0;
+  form!: FormGroup;
+
   constructor(
+    // private form: FormGroup,
     private router: Router,
     private service: ServiceService,
     private fb: FormBuilder
   ) {}
 
-  ngOnInit(): void {}
-
-  onSubmit(): void {
-    // let usuario1 = new Usuario(
-    //   this.nombreInput,
-    //   this.emailInput,
-    //   this.prioridadInput
-    // );
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      nombre: '',
+      email: '',
+      prioridad: 0,
+    });
   }
 
-  // initForm(): void {
-  //   this.form = new FormGroup({
-  //     nombre: new FormControl(),
-  //     email: new FormControl(),
-  //     prioridad: new FormControl(),
-  //   });
-  // }
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
 }
